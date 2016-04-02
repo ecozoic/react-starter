9a2c99fec000c6ef950f3ab6b84cd2bd9f9e50f9
ecozoic <johnnn.mitchell@gmail.com>
2016-04-02 18:17:00 -0400

    add shims + separate vendor code into separate webpack bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,25 @@
 var path = require('path');
 
 module.exports = {
-  context: path.resolve('src/'),
-  entry: ['./index'],
+  entry: {
+    app: './src/index',
+    vendor: [
+      'es5-shim',
+      'es6-shim',
+      'es6-promise',
+      'react',
+      'react-dom',
+      'react-router'
+    ]
+  },
 
   output: {
-    path: path.resolve('dist/'),
+    path: path.resolve(__dirname, 'dist'),
     publicPath: '/public/assets/',
-    filename: 'app.js'
+    filename: '[name].js'
   },
+
+  devtool: 'source-map',
 
   devServer: {
     contentBase: 'public',
