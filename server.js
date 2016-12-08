d5c5dcc7f6d9e3db04be14c218af876c3d4e8e50
ecozoic <johnnn.mitchell@gmail.com>
2016-12-08 11:03:17 -0500

    update deps + refactor dist dir structure + add caching and etags

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const koa = require('koa');
 
 const compress = require('koa-compress');
+const conditional = require('koa-conditional-get');
+const etag = require('koa-etag');
 const favicon = require('koa-favicon');
 const logger = require('koa-logger');
+const mount = require('koa-mount');
 const send = require('koa-send');
 const serve = require('koa-static');
 
@@ -14,11 +17,18 @@ app.use(favicon(`${__dirname}/dist/favicon.ico`));
 
 app.use(logger());
 
-// TODO: caching
-app.use(serve('./dist'));
+app.use(conditional());
+app.use(etag());
+
+
+app.use(mount('/assets', serve('dist/assets', {
+  maxage: 365 * 24 * 60 * 60 * 1000
+})));
 
 app.use(function*() {
-  yield send(this, 'dist/index.html');
+  yield send(this, 'dist/index.html', {
+    maxage: 0
+  });
 });
 
 const port = process.env.PORT || 8080;
