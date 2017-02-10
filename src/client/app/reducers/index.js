00193bbbdcdd41b3fdd49687ec0bf0def6eef8c6
ecozoic <johnnn.mitchell@gmail.com>
2017-02-10 10:13:13 -0500

    add redux-form

--- src/client/app/reducers/index.js
+++ src/client/app/reducers/index.js
@@ -1,10 +1,13 @@
 /* @flow */
 import { combineReducers } from 'redux-immutable';
+import { reducer as form } from 'redux-form/immutable';
+
 import { todos } from './todos';
 
 /**
  * Root reducer
  */
 export const rootReducer = combineReducers({
-  todos
+  todos,
+  form
 });
--- src/client/vendor.js
+++ src/client/vendor.js
@@ -4,13 +4,15 @@ import 'react';
 import 'react-dom';
 import 'react-router-dom';
 
+import 'history';
+import 'immutable';
+
 // Redux
 import 'redux';
 import 'react-redux';
-import 'history';
-import 'connected-react-router';
-import 'immutable';
+import 'connected-react-router/immutable';
 import 'redux-immutable';
 import 'redux-thunk';
+import 'redux-form/immutable';
 
 import 'normalize.css';
