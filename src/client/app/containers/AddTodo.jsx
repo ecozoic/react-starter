f2d17ac8da2b98df5cb0ce1f23ee0c5e164f06cd
John Mitchell <john.mitchell@ignitionone.com>
2017-04-12 11:11:00 -0400

    upgrade to react 15.5

--- src/client/app/containers/AddTodo.jsx
+++ src/client/app/containers/AddTodo.jsx
@@ -1,5 +1,6 @@
 /* @flow */
-import React, { PropTypes, Component } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
