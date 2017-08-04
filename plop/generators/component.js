b394af784adb3b8885128ecf1b6f1a9037c319eb
ecozoic <johnnn.mitchell@gmail.com>
2017-08-04 19:04:57 -0400

    finish component generator

--- plop/generators/component.js
+++ plop/generators/component.js
@@ -4,18 +4,54 @@ module.exports = {
     type: 'input',
     name: 'name',
     message: 'component name please',
-  }],
-  actions: [{
-    type: 'add',
-    path: 'src/app/components/{{name}}/{{name}}.tsx',
-    templateFile: 'plop/templates/component/stateless.txt',
   }, {
-    type: 'add',
-    path: 'src/app/components/{{name}}/index.ts',
-    templateFile: 'plop/templates/component/index.txt',
+    type: 'confirm',
+    name: 'stateless',
+    message: 'is the component stateless',
+    default: true,
   }, {
-    type: 'add',
-    path: 'src/app/components/{{name}}/{{name}}.spec.tsx',
-    templateFile: 'plop/templates/component/spec.txt',
+    type: 'confirm',
+    name: 'styles',
+    message: 'does the component need styles',
+    default: true,
   }],
+  actions: (data) => {
+    const actions = [];
+
+    if (data.stateless) {
+      actions.push({
+        type: 'add',
+        path: 'src/app/components/{{name}}/{{name}}.tsx',
+        templateFile: 'plop/templates/component/stateless.txt',
+      });
+    } else {
+      actions.push({
+        type: 'add',
+        path: 'src/app/components/{{name}}/{{name}}.tsx',
+        templateFile: 'plop/templates/component/stateful.txt',
+      });
+    }
+
+    if (data.styles) {
+      actions.push({
+        type: 'add',
+        path: 'src/app/components/{{name}}/{{name}}.scss',
+        templateFile: 'plop/templates/component/styles.txt',
+      });
+    }
+
+    actions.push({
+      type: 'add',
+      path: 'src/app/components/{{name}}/index.ts',
+      templateFile: 'plop/templates/component/index.txt',
+    });
+
+    actions.push({
+      type: 'add',
+      path: 'src/app/components/{{name}}/{{name}}.spec.tsx',
+      templateFile: 'plop/templates/component/spec.txt',
+    });
+
+    return actions;
+  },
 };
