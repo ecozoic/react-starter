38d6e73ee168b15283eb0aab3537b12ad72e265c
ecozoic <johnnn.mitchell@gmail.com>
2018-09-17 12:42:56 -0400

    build(babel): configure babel to only transform modules in test env

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,17 @@
 module.exports = function(api) {
   const env = api.env();
 
-  // TODO: configure env based on browsers/polyfills
-  // TODO: disable modules so webpack can tree-shake
-  // TODO: ensure modules still transformed when running jest
+  // TODO: configure env based on browsers (browserslistrc)
+  // TODO: polyfills
+  // TODO: stage-x plugins/presets
   // TODO: lodash
 
   return {
-    presets: ['@babel/env', '@babel/typescript', '@babel/react'],
+    presets: [
+      ['@babel/env', { modules: env === 'test' ? 'commonjs' : false }],
+      '@babel/typescript',
+      '@babel/react',
+    ],
     plugins: ['styled-components', 'react-hot-loader/babel'],
   };
 };
